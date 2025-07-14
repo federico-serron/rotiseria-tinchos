481c581cbc0c8fc18cf55bc0025b391f5b7e4be7
federico-serron <f.serron.umpierrez@gmail.com>
2025-07-14 18:57:58 +0930

    Set up config.py so we can change enviroment dev, prod and testing

---
+++ backend/app/config.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    WTF_CSRF_ENABLED = False
+    
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    DEBUG = False
