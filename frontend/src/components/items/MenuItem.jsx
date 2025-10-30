325e7e377d15374e6fa952ced7dfc4c4c9ca9862
federico-serron <f.serron.umpierrez@gmail.com>
2025-10-30 16:25:34 +0930

    Card styles improved for better UX

--- frontend/src/components/items/MenuItem.jsx
+++ frontend/src/components/items/MenuItem.jsx
@@ -4,7 +4,7 @@ import { AddToCart } from './AddToCart.jsx'
 export const MenuItem = ({item}) => {
     return (
         <div className="col-sm-6 col-lg-4 all" data-category={item.category}>
-            <div className="box">
+            <div className="box menu-item-card">
                 <div>
                     <div className="img-box">
                         <img className='img-fluid w-100' style={{objectFit:'cover'}} src={item.path ? `${import.meta.env.VITE_BACKEND_URL}/${item.path}` : 'https://cdn.pixabay.com/photo/2014/12/21/23/36/burgers-575655_1280.png'} alt={item.name} />
